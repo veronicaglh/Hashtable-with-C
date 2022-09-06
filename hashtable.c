88d54bc3778b5f81f2dec77c590170531041e97c
veronicaglh <veronicagt.IT@gmail.com>
2022-09-06 07:09:22 +0300

    add while loop to find matching key

--- hashtable.c
+++ hashtable.c
@@ -140,3 +140,43 @@ void ht_del(ht_t *hashtable, const char *key) {
 
     entry_t *prev;
     int idx = 0;
+
+    // walk through each entry until either the end is reached or a matching key is found
+    while (entry != NULL) {
+        // check key
+        if (strcmp(entry->key, key) == 0) {
+            // first item and no next entry
+            if (entry->next == NULL && idx == 0) {
+                hashtable->entries[bucket] = NULL;
+            }
+
+            // first item with a next entry
+            if (entry->next != NULL && idx == 0) {
+                hashtable->entries[bucket] = entry->next;
+            }
+
+            // last item
+            if (entry->next == NULL && idx != 0) {
+                prev->next = NULL;
+            }
+
+            // middle item
+            if (entry->next != NULL && idx != 0) {
+                prev->next = entry->next;
+            }
+
+            // free the deleted entry
+            free(entry->key);
+            free(entry->value);
+            free(entry);
+
+            return;
+        }
+
+        // walk to next
+        prev = entry;
+        entry = prev->next;
+
+        ++idx;
+    }
+}
