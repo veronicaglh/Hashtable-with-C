e0a6fdd52307530801954fd767ece23bb8a574b6
veronicaglh <veronicagt.IT@gmail.com>
2022-09-06 07:11:50 +0300

    add dump function for table

--- hashtable.c
+++ hashtable.c
@@ -180,3 +180,27 @@ void ht_del(ht_t *hashtable, const char *key) {
         ++idx;
     }
 }
+
+void ht_dump(ht_t *hashtable) {
+    for (int i = 0; i < TABLE_SIZE; ++i) {
+        entry_t *entry = hashtable->entries[i];
+
+        if (entry == NULL) {
+            continue;
+        }
+
+        printf("slot[%4d]: ", i);
+
+        for(;;) {
+            printf("%s=%s ", entry->key, entry->value);
+
+            if (entry->next == NULL) {
+                break;
+            }
+
+            entry = entry->next;
+        }
+
+        printf("\n");
+    }
+}
