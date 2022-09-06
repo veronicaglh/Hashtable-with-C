bf53ca682e979a573ed01d0cd4ddc238a499dbd8
veronicaglh <veronicagt.IT@gmail.com>
2022-09-06 07:01:08 +0300

    allocate the entries of the table

--- hashtable.c
+++ hashtable.c
@@ -16,3 +16,51 @@ typedef struct entry_t {
 typedef struct {
     entry_t **entries;
 } ht_t;
+
+unsigned int hash(const char *key) {
+    unsigned long int value = 0;
+    unsigned int i = 0;
+    unsigned int key_len = strlen(key);
+
+    // do several rounds of multiplication
+    for (; i < key_len; ++i) {
+        value = value * 37 + key[i];
+    }
+
+    // make sure value is 0 <= value < TABLE_SIZE
+    value = value % TABLE_SIZE;
+
+    return value;
+}
+
+entry_t *ht_pair(const char *key, const char *value) {
+    // allocate the entry
+    entry_t *entry = malloc(sizeof(entry_t) * 1);
+    entry->key = malloc(strlen(key) + 1);
+    entry->value = malloc(strlen(value) + 1);
+
+    // copy the key and value in place
+    strcpy(entry->key, key);
+    strcpy(entry->value, value);
+
+    // next starts out null but may be set later on
+    entry->next = NULL;
+
+    return entry;
+}
+
+ht_t *ht_create(void) {
+    // allocate table
+    ht_t *hashtable = malloc(sizeof(ht_t) * 1);
+
+    // allocate table entries
+    hashtable->entries = malloc(sizeof(entry_t*) * TABLE_SIZE);
+
+    // set each to null (needed for proper operation)
+    int i = 0;
+    for (; i < TABLE_SIZE; ++i) {
+        hashtable->entries[i] = NULL;
+    }
+
+    return hashtable;
+}
