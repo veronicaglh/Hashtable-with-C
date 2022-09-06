ca6ccaf9c48c90efed94dd7853863dcca3fd78b1
veronicaglh <veronicagt.IT@gmail.com>
2022-09-06 06:58:36 +0300

    declare necessary variables and create struct

--- hashtable.c
+++ hashtable.c
@@ -1,1 +1,18 @@
 /*This program will implement the hash table data structure. Using C language*/
+
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TABLE_SIZE 20000
+
+typedef struct entry_t {
+    char *key;
+    char *value;
+    struct entry_t *next;
+} entry_t;
+
+typedef struct {
+    entry_t **entries;
+} ht_t;
