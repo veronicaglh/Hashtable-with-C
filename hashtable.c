57b505f2abd6c9e05fb9a2bab3247db2e65eb3c0
veronicaglh <veronicagt.IT@gmail.com>
2022-09-06 07:15:09 +0300

    pass the first round of key-value pairs to set function

--- hashtable.c
+++ hashtable.c
@@ -204,3 +204,19 @@ void ht_dump(ht_t *hashtable) {
         printf("\n");
     }
 }
+
+int main(int argc, char **argv) {
+    ht_t *ht = ht_create();
+
+    ht_set(ht, "name1", "em");
+    ht_set(ht, "name2", "russian");
+    ht_set(ht, "name3", "pizza");
+    ht_set(ht, "name4", "doge");
+    ht_set(ht, "name5", "pyro");
+    ht_set(ht, "name6", "joost");
+    ht_set(ht, "name7", "kalix");
+
+    ht_dump(ht);
+
+    return 0;
+}
